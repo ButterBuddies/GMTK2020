012e4968b229dc48c597a530547de64f8dd007ca
Regretful123 <jordbej@yahoo.com>
2020-07-10 13:31:37 -0500

    Added GameJam Logo animation
    Added game jam logo animation + scripts to load next scene. - JB

---
+++ Assets/scripts/SceneManagement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadScene(int buildIndex )
+    {
+        try
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+        catch
+        {
+            Debug.LogError("Did you forgot to add the scene to the build manager?");
+        }
+        
+    }
+
+    public void LoadScene(string SceneName)
+    {
+        try
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+        catch
+        {
+            Debug.LogError("Did you misspell or did you check and see if the scene name exist in the build manager?");
+        }
+    }
+
+    /// <summary>
+    /// Loads the next scene in order of the build index.
+    /// </summary>
+    public void LoadNextScene()
+    {
+        try
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        catch
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/scripts/logic_timer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class logic_timer : MonoBehaviour
+{
+    public bool PlayOnAwake = false;
+    public float Interval = 5f;
+    public bool IsLoop = false;
+    public UnityEvent OnTimer;
+
+    [SerializeField]
+    public float _timer { get; private set; }
+    private bool _isPlaying = false;
+
+    private void Awake()
+    {
+        if (PlayOnAwake)
+            Start();
+    }
+
+    public void Start()
+    {
+        _isPlaying = true;
+    }
+
+    public void Stop()
+    {
+        _isPlaying = false;
+    }
+
+    public void Reset()
+    {
+        _isPlaying = false;
+        _timer = 0;
+    }
+
+    public void Update()
+    {
+        if (!_isPlaying) return;
+
+        _timer += Time.deltaTime;
+        if(_timer > Interval)
+        {
+            OnTimer.Invoke();
+            if (IsLoop)
+                _timer = 0;
+            else
+                _isPlaying = false;
+        }
+    }
+}
