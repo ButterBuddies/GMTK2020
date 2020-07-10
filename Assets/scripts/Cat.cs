e4a0f9a391fa614172e3a498d3e2d074ce123ff1
Regretful123 <jordbej@yahoo.com>
2020-07-10 17:13:16 -0500

    Added Cats into test scene
    Added cats into test scene

--- Assets/scripts/Cat.cs
+++ Assets/scripts/Cat.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Cat : MonoBehaviour
 {
+    #region variables
     public enum Behavior { Flee, Chase, Idle, Freeze }
     //public enum Emotion { Lonely, Scared, Happy, Clean, Scout, Sleep, Eating}
 
@@ -11,10 +12,15 @@ public class Cat : MonoBehaviour
     public float speed = 5f;
     // if the cat weights gets too much, then thespeed of the cat and the emotion of the cat gets inflicted. 
     public float weight = 1f;
+    // Time it takes for cat to change it's behavior decisions beacse you know.. cats get weird and strange to predict?
+    public float timeToChangeDecision = 20.0f;
     // Hmm.... over the time if the cat consume too much then the weight becomes a factor
     [Range(0,1)]
     public float foodConsumption = 0.5f;
     // if the cat is losing weight then the speed and performance of the cat gets afected. affects tolerance and attention span?
+
+    public float MaxRadius = 10f;
+    public float MinRadius = 2f;
 
     public Behavior CurrentBehavior = Behavior.Idle;
     public float AttentionSpan = 10f;
@@ -24,6 +30,10 @@ public class Cat : MonoBehaviour
 
     // in case the cat have found a target to aim for.
     private GameObject _suspectedTarget;
+    private NavMeshAgent _agent;
+    private float _t = 0;
+
+#endregion
 
     #region Implementations
     public void MoveTowards( GameObject obj )
@@ -77,6 +87,34 @@ public class Cat : MonoBehaviour
 
     #region Behavior engine
 
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Show the max area of which the range is given to the cat.
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, MaxRadius);
+        Gizmos.DrawWireSphere(transform.position, MinRadius);
+
+        if ( _agent == null ) return;
+        Vector3[] v = _agent.path.corners;
+        Gizmos.color = Color.yellow;
+        if (v.Length == 1)
+        {
+            Gizmos.DrawLine(this.transform.position, v[0]);
+        }
+        else
+        {
+            for (int i = 1, n = v.Length - 1; i < n - 1; i++)
+            {
+                Gizmos.DrawLine(v[i - 1], v[i]);
+            }
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -89,9 +127,29 @@ public class Cat : MonoBehaviour
             default: // hmm you did something wrong to make this happen shame on you and yoru code design... 
                 break;
         }
+        _t += Time.deltaTime;
+
+        if( timeToChangeDecision < _t )
+        {
+            _t = 0;
+            // somehow we're going to randomized the cat's behavior here?
+            //float i = Random.Range(0, 2);
+            //if (i < 1)
+            //{
+            //    CurrentBehavior = Behavior.Idle;
+            //}
+            //else
+            //{
+                // let's make it interesting? Let's find a marker somewhere on the map and pick it random?
+                float x = Mathf.Sin(Random.Range(-360.0f, 360.0f)) * Random.Range(MinRadius, MaxRadius) + transform.position.x;
+                float z = Mathf.Cos(Random.Range(-360.0f, 360.0f)) * Random.Range(MinRadius, MaxRadius) + transform.position.z;
+                Vector3 dir = new Vector3(x, this.transform.position.y + 10f, z);
+                CurrentBehavior = Behavior.Chase;
+                // in this case we want the cat to be just walking.... instead of chasing?
+                _agent.SetDestination(dir);
+            //}
+        }
     }
-    
-
-
+   
     #endregion
 }
