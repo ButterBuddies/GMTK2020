509d8d21bb816c4248f3bab87ad1feed941c4646
Regretful123 <jordbej@yahoo.com>
2020-07-11 19:49:33 -0500

    Added laser script
    Added laser script object to control cats

--- Assets/scripts/LaserPointer.cs
+++ Assets/scripts/LaserPointer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class LaserPointer : Item
 {
     public float AttentionRadius = 10.0f;
@@ -9,35 +10,85 @@ public class LaserPointer : Item
     public float rechargeRate = 1f;
     public float delayToRecharge = 1f;
     private float _t;
-    public enum State { Using, Idle, Recharging, Dead }
-    public State state = State.Idle;
-
     public GameObject Pointers;
 
     // Hmmm..... interesting?
-    public bool isOn = false;
+    private bool _isOn = false;
+    private LineRenderer _laserBeam;
 
-    // add
+    private void Start()
+    {
+        _t = 0;
+        _laserBeam = GetComponent<LineRenderer>();
+    }
 
     public void Update()
     {
-        isOn = Input.GetButton("Fire");
+        _isOn = Input.GetButton("Fire1");
     }
 
     public void FixedUpdate()
     {
-        if (state == State.Dead)
+        if ( BatteryLife <= 0 )
         {
-            isOn = false;
+            _isOn = false;
         }
 
-        if ( isOn )
+        if ( _isOn )
         {
-            
+            _t = 0;
+            // drain battery life for usage
+            BatteryLife -= usageDrain * Time.fixedDeltaTime;
+            if ( Pointers?.activeSelf == false )
+            {
+                Pointers?.SetActive(true);
+                _laserBeam.enabled = true;
+            }
+
+            // assign location and position of where the laser will hit.
+            // set end point of the laser
+            Vector3 endPoint = transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(this.transform.position, this.transform.forward * 1000.0f, out hit ) )
+            {
+                endPoint = hit.point;
+                if (Pointers != null)
+                {
+                    Pointers.transform.position = hit.point + hit.normal * 0.01f;
+                    Pointers.transform.rotation = Quaternion.Euler(hit.normal);   // test and see if this works.
+                }
+            }
+            else
+            {
+                endPoint = transform.forward * 1000f;
+                if (Pointers?.activeSelf == true )
+                {
+                    Pointers?.SetActive(false);
+                }
+                // just draw the trail render instead to a straight vertical line...
+            }
+            // update the trail regardless of the condition above...
+            _laserBeam.SetPosition( 0, transform.position );
+            _laserBeam.SetPosition( _laserBeam.positionCount - 1, endPoint );
         }
         else
         {
+            // turn off laser if they're still on?
+            if ( Pointers != null && Pointers.activeSelf )
+            {
+                Pointers.SetActive(false);
+                _laserBeam.enabled = false;
+            }
 
+            // recharge battery life
+            if (BatteryLife < 100f)
+            {
+                _t += Time.fixedDeltaTime;
+                if ( _t > delayToRecharge)
+                {
+                    BatteryLife += Time.fixedDeltaTime * rechargeRate;
+                }
+            }
         }
     }
 
