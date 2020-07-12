6e5c84c48eb1030f408f4499aee0cdeb02231a31
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-12 06:59:33 -0700

    push scritpts

---
+++ Assets/scripts/TutorialProgress.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Playables;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialProgress : MonoBehaviour
+{
+    PlayerController controller;
+   [SerializeField] PlayableDirector director;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ChangeScene();
+        }
+        if (controller.progress > 2)
+        {
+            if (director.state != PlayState.Playing)
+                director.Play();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("The Main scene");
+    }
+}
